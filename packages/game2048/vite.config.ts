29a3f5c1326cf96b036b8ed34ac78b632d74f50f
xiongjie <jiexiong@futunn.com>
2022-10-25 00:00:31 +0800

    安装 react

--- packages/game2048/vite.config.ts
+++ packages/game2048/vite.config.ts
@@ -1,6 +1,7 @@
 import wasm from "vite-plugin-wasm";
 import { defineConfig } from "vite";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 export default defineConfig({
-  plugins: [wasm()],
+  plugins: [wasm(), vanillaExtractPlugin()],
 });
